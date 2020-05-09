56d101c552ffb58e8ebb0ca2bc72d364000f176e
Zach <zachery.goodrich@gmail.com>
2020-05-09 12:38:19 -0700

    hello world

---
+++ views/index.jsx
@@ -0,0 +1,13 @@
+const React = require('react');
+
+class Index extends React.Component {
+    render() {
+        return(
+            <body>
+                <h1>To Do List</h1>
+            </body>
+        )
+    }
+}
+
+module.exports = Index;
